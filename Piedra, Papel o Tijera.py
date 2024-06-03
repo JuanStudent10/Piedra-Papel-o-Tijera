22cf5867bbf768aeb90116c6fd5c6ef3f7a33f7b
JuanStudent10 <juanmatallana2010@gmail.com>
2024-06-02 21:58:21 -0500

    Avance en la codificación del juego: Piedra, Papel o Tijera.
    Se adjunta archivo de Python sobre el avance en la codificación del juego desarrollado en Visual Studio Code, esperando realizar correcciones y mejoras en su desarrollo, así como agregar más opciones de juego de manera continua.

---
+++ Piedra, Papel o Tijera.py
@@ -0,0 +1,17 @@
+jugador=input("Elige piedra, papel o tijera: ")
+computadora="piedra"  # La computadora elige piedra por defecto
+
+print("La computadora elige:", computadora)
+
+# Resultados predefinidos
+if jugador == "piedra" and computadora == "tijera":
+ print("Ganaste!")
+elif jugador == "papel" and computadora == "piedra":
+ print("Ganaste!")
+elif jugador == "tijera" and computadora == "papel":
+ print("Ganaste!")
+elif jugador == computadora:
+ print("Empate!")
+else:
+ print("La computadora gana!")
+    
